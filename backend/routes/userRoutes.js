5994fffd8559938e66fa056c87f5215a30efbdf2
Jayesh Shaha <106771749+jayeshshaha@users.noreply.github.com>
2024-02-02 23:29:11 +0530

    USER ROUTES DONE

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,19 @@
 import express from "express";
-import { createUser, loginUser,logoutCurrentUser, getAllUsers } from "../controllers/userController.js";
-import {authenticated,authorizedAdmin} from "../middlewares/authMiddleware.js";
-// import {authenticated, authorizedAdmin} from "../middlewares/authMiddleware.js";
+import { createUser, loginUser,logoutCurrentUser, getAllUsers, getCurrentUserProfile,updateCurrentUserProfile } from "../controllers/userController.js";
 
+
+import {authenticated,authorizedAdmin} from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(createUser).get(authenticated,authorizedAdmin,getAllUsers)
 router.post("/auth",loginUser);
 router.post("/logout",logoutCurrentUser);
+
+
+router.route("/profile").get(authenticated,getCurrentUserProfile).put(authenticated,updateCurrentUserProfile)
+
+// ADMIN RELATED ROUTES ------------->
 
 
 export default router;
